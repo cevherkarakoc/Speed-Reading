36f55fb3dfa9ad4e8db92ccc6b37c32deab31c73
Cevher Karakoç <cevherkarakoc@live.com>
2021-04-10 11:26:37 +0300

    update word font-size

--- src/components/TheWord.js
+++ src/components/TheWord.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TheWord = ({word, pausePlay}) => {
   return (
       <div className="flex justify-center items-center h-screen" onClick={pausePlay}>
-        <h1 className="text-3xl">{word}</h1>
+        <h1 className="text-4xl">{word}</h1>
       </div>)
 }
 
