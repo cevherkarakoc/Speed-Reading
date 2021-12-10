a125ea48637b583d886b5b2757e6946a3c11b16c
Cevher Karakoç <cevherkarakoc@live.com>
2021-12-10 20:09:12 +0300

    update parcel and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    purge: [
+    content: [
         './src/**/*.html',
         './src/**/*.js',
       ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
     },
-    variants: {},
     plugins: [],
 }
